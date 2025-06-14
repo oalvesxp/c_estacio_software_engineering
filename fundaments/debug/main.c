097349510fe4cd9850298ed631431786e130044b
Osvaldo Alves <oalves.neto@outlook.com>
2025-06-14 11:20:48 -0300

    feat: how to debug programs in C by VSCode

---
+++ fundaments/debug/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** run this program using the console pauser or add your own getch */
+int main(int argc, char *argv[]){
+  int i = 3;
+
+  printf("Hello world!\n");
+  printf("Welcome to my program!\n");
+  printf("This is a debug exercise!\n");
+  
+  printf("I = %i", i);
+  printf("End program");
+
+  return 0;
+}

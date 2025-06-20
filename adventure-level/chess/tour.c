5fc0fcd217fd32ae62e3a89106c1d5dcf3cda9a1
Osvaldo Alves <oalves.neto@outlook.com>
2025-06-20 15:44:11 -0300

    feat: add comments

--- adventure-level/chess/tour.c
+++ adventure-level/chess/tour.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-  int y = 3;
-  char x[] = "ABCDEFGH";
+  int y = 3;              // lines
+  char x[] = "ABCDEFGH";  // columns
 
   printf("Movendo a Torre de A3 para H3...\n");
 
+  int START = 0;  // index of column A
+  int END = 7;    // index of column H
 
-  int START = 0;
-  int END = 7;
   for(int i = START; i <= END; i++)
   {
     printf("%c%d\n",x[i], y);

85f06728e94f59736fdede748b036a9169454504
Osvaldo Alves <oalves.neto@outlook.com>
2025-06-20 15:43:30 -0300

    feat: convert looping (while)

--- adventure-level/chess/bishop.c
+++ adventure-level/chess/bishop.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-  int y = 1;
-  char x[] = "ABCDEFGH";
+  int y = 1;             // lines
+  char x[] = "ABCDEFGH"; // columns
 
   printf("Movendo o Bispo de C1 para H6...\n");
 
-  int START = 2;
-  int END = 7;
-  for(int i = START; i <= END; i++)
+  int i = 2;    // index of column C
+  int END = 7;  // index of column H
+
+  while (i <= END)
   {
-    printf("%c%d\n",x[i], y);
+    printf("%c%d\n", x[i], y);
+    i++;
     y++;
   };
 

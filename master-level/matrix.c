38b06bf9068d88ba6306a634d7abe5744b511ebd
Osvaldo Alves <oalves.neto@outlook.com>
2025-06-28 15:39:29 -0300

    feat: add matrix with nested loops and conditionals

--- master-level/matrix.c
+++ master-level/matrix.c
@@ -2,8 +2,7 @@
 
 /**
  * Matrizes em C
- */
-
+*/
 int main(void) {
   int matrix[3][3] = {{1, 2, 3},{4, 5, 6}, {7, 8, 9}};
   int pairCount = 0;  // contador de pares 
@@ -24,6 +23,24 @@ int main(void) {
 
   printf("Total de elementos pares: %d\n", pairCount);
   printf("Total de elementos impares:  %d\n", oddCount);
+
+  /**
+   * Modificação em matriz por condicional
+  */
+  for(int row = 0; row < 3; row++)
+  {
+    for(int col = 0; col < 3; col++)
+    {
+      if(matrix[row][col] > 5)  // vai alterar o conteúdo da matriz se o valor for mair que 5
+      {
+        matrix[row][col] = -matrix[row][col]; // atribui o valor negativo
+      }
+
+      printf("%d ", matrix[row][col]);
+    }
+
+    printf("\n");
+  }
 
   return 0;
 }

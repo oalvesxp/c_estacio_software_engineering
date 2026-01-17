84871412591b35a2ef26593c55b54e6b77c441e9
Osvaldo Alves <osvaldo@if.ind.br>
2026-01-17 13:54:54 -0300

    refactor: simplify main function by removing unused queue and reserved parts logic

--- 05_data_structure/challenge/game-tetris/main.c
+++ 05_data_structure/challenge/game-tetris/main.c
@@ -1,46 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "modules/tetrisParts/part.h"
-#include "modules/circularQueue/queue.h"
-#include "modules/reservedParts/reserved.h"
+
+#include "modules/game/game.h"
 
 int
 main(void) {
   srand(time(NULL)); // semente para números aleatórios
-
-  CircularQueue queue;
-  initQueue(&queue);
-
-  ReservedParts reservedParts;
-  initReservedParts(&reservedParts);
-
-  Part p;
-  for(int i = 1; i <= 5; i++){
-    generatePart(&p);
-    enqueue(&queue, p);
-  };
-
-  printf("\n");
-  dequeue(&queue, &p);
-  dequeue(&queue, &p);
-
-  reservePartFromQueue(&queue, &p, &reservedParts);
-  showReservedParts(&reservedParts);
-
-  reservePartFromQueue(&queue, &p, &reservedParts);
-  showReservedParts(&reservedParts);
+  GameState gameState;
   
-  reservePartFromQueue(&queue, &p, &reservedParts);
-  showReservedParts(&reservedParts);
+  initGame(&gameState);
 
-  reservePartFromQueue(&queue, &p, &reservedParts);
-  showReservedParts(&reservedParts);
-
-  pop(&reservedParts, &p);
-  showReservedParts(&reservedParts);
-
-  reservePartFromQueue(&queue, &p, &reservedParts);
-  showReservedParts(&reservedParts);
   return 0;
 }

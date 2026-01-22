6a17b03211411360ddfde8a624cd16791bf57f30
Osvaldo Alves <osvaldo@if.ind.br>
2026-01-21 21:13:53 -0300

    feat: update main program to include menuPrint and refactor item initialization feat: add game state structure for managing game items and initialization status

--- 05_data_structure/challenge/game-bag/mestre/main.c
+++ 05_data_structure/challenge/game-bag/mestre/main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include "modules/item/item.h"
+#include "modules/menu/menu.h"
 
 int
 main(void){
-    printf("Hello, Game Bag!\n");
+    // printf("Hello, Game Bag!\n");
 
     Bag bag;
     initBag(&bag);
 
-    Item a = {"Health Potion", "Consumable", 5, 1};
-    Item b = {"Iron Sword", "Weapon", 1, 2};
-    Item c = {"Wooden Shield", "Armor", 1, 3};
+    menuPrint();
 
-    pushItemToBag(&bag, a);
+    // Item a = {"Health Potion", "Consumable", 5, 1};
+    // Item b = {"Iron Sword", "Weapon", 1, 2};
+    // Item c = {"Wooden Shield", "Armor", 1, 3};
+
+    // pushItemToBag(&bag, a);
     
-    showBagItems(&bag);
+    // showBagItems(&bag);
 
-    pushItemToBag(&bag, b);
-    pushItemToBag(&bag, c);
-    showBagItems(&bag);
+    // pushItemToBag(&bag, b);
+    // pushItemToBag(&bag, c);
+    // showBagItems(&bag);
 
     return 0;
 }
